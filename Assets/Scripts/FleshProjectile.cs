db70d966814233f128621fa985be0d95e0bd763e
CornBoa <153493573+CornBoa@users.noreply.github.com>
2024-05-30 14:15:51 +0300

    important stuff

---
+++ Assets/Scripts/FleshProjectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleshProjectile : MonoBehaviour
+{
+    public Transform Target;
+    public enum KindOfProjectile
+    {
+        Teeth,
+        Rot,
+        Napalm
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -1,55 +1,117 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WaveManager : MonoBehaviour
 {
+    public Wave currentWave;
     public List<Wave> waves;
-    Wave currentWave;
+    Group currentGroup;
     private float[] nextSpawnTimes;
     public Transform spawnPosition;
-    float minX, maxX;
     public List<MapLayout> layouts;
     MapLayout currentLayout;
+    public float timeBetweenWaves;
+    int waveIndex = 0;
+    int groupIndex = 0;
+    public bool WaveGoing = false;
     void Start()
     {
         currentLayout = layouts[0];
-       currentWave = waves[0];
-        nextSpawnTimes = new float[currentWave.enemyPrefabs.Length];
+        currentWave = waves[waveIndex];
+        currentGroup = waves[waveIndex].groupsOfWave[groupIndex];
+        nextSpawnTimes = new float[currentGroup.enemyPrefabs.Count];
         for (int i = 0; i < nextSpawnTimes.Length; i++)
         {
-            nextSpawnTimes[i] = Time.time + currentWave.spawnIntervals[i];
+            nextSpawnTimes[i] = Time.time + currentGroup.spawnIntervals[i];
         }
         spawnPosition = currentLayout.transformList[0];
     }
     void Update()
     {
-        for (int i = 0; i < currentWave.enemyPrefabs.Length; i++)
+        if (WaveGoing)
         {
-            if (Time.time >= nextSpawnTimes[i])
+            for (int i = 0; i < currentGroup.enemyPrefabs.Count; i++)
             {
-                SpawnEnemy(i);
-                nextSpawnTimes[i] = Time.time + currentWave.spawnIntervals[i];
+                if (Time.time >= nextSpawnTimes[i])
+                {
+                    SpawnEnemy(i);
+                    Debug.Log("Pee");
+                    nextSpawnTimes[i] = Time.time + currentGroup.spawnIntervals[i];
+                }
             }
         }
     }
+    public IEnumerator TimeBetweenGroups()
+    {
+           if (groupIndex < waves[waveIndex].groupsOfWave.Length-1)
+           {
+                yield return new WaitForSeconds(waves[waveIndex].groupsOfWave[groupIndex].timeBeforeNextGroup);
+                groupIndex++;
+                currentGroup = waves[waveIndex].groupsOfWave[groupIndex];
+           }
+           else
+           {
+                WaveGoing = false;
+                Debug.Log("OnWaveEndCalled");
+                currentWave.OnWaveEnd.Invoke();
+           }       
+    }
+    public void StartWaveEnum()
+    {
+        Debug.Log("WaveCooldownStarted");
+        StartCoroutine(StartNextWave());
+    }
+    public IEnumerator StartNextWave()
+    {
+        yield return new WaitForSeconds(timeBetweenWaves);
+        waveIndex++;
+        groupIndex = 0;
+        currentWave = waves[waveIndex];
+        currentGroup = waves[waveIndex].groupsOfWave[groupIndex];
+        WaveGoing = true;
 
+    }
     void SpawnEnemy(int index)
     {
-        if (currentWave.spawnAmount[index] > 0)
+        if (currentGroup.spawnAmount[index] > 0)
         {
-            IEnemy enemy = Instantiate(currentWave.enemyPrefabs[index], spawnPosition.position, Quaternion.identity).GetComponent<IEnemy>();
+            IEnemy enemy = Instantiate(currentGroup.enemyPrefabs[index], spawnPosition.position, Quaternion.identity).GetComponent<IEnemy>();
             enemy.SetLayout(currentLayout);
-            currentWave.spawnAmount[index]--;
+            currentGroup.spawnAmount[index]--;
         }   
+        else if(currentGroup.spawnAmount[index] == 0)
+        {
+            currentGroup.enemyPrefabs.RemoveAt(index);
+            StartCoroutine(TimeBetweenGroups());
+        }
     }
 }
 [System.Serializable]
 public class Wave
 {
-    public GameObject[] enemyPrefabs;
+    public Group[] groupsOfWave;
+    public UnityEvent OnWaveEnd;
+    void StartWave()
+    {
+        foreach (var group in groupsOfWave)
+        {
+            group.mamaWave = this;
+        }
+    }
+}
+
+[System.Serializable]
+public class Group
+{
+    public List<GameObject> enemyPrefabs;
     public float[] spawnIntervals;
     public int[] spawnAmount;
+    public float timeBeforeNextGroup;
+    public Wave mamaWave;
 }
 [System.Serializable]
 public class MapLayout
